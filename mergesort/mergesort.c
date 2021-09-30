f4bac0e23b69c7c4f6aebd0ee24f08968ecd8c2a
Kyle Day <day00090@stardust.morris.umn.edu>
2021-09-30 10:53:03 -0500

    Add mergeRanges Function to mergesort.c
    This commit incorporates the function mergeRanges into mergesort.c. It
    will be useful with merging the array ranges that this file deals with.
    
    Co-authored-by: Jeffrey Miller mill7707@morris.umn.edu

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -16,6 +16,45 @@ void mergesortRange(int values[], int startIndex, int endIndex) {
   }
 
 void mergeRanges(int values[], int startIndex, int midPoint, int endIndex) {
+  const int rangeSize = endIndex - startIndex;
+  int destination[] = new int[rangeSize];
+  int firstIndex = startIndex;
+  int secondIndex = midPoint;
+  int copyIndex = 0;
+
+  while(firstIndex < midPoint && secondIndex < endIndex) {
+    if (values[firstIndex] < values[secondIndex]) {
+      destination[copyIndex] = values[firstIndex];
+      ++firstIndex;
+    }
+    
+    else {
+      destination[copyIndex] = values[firstIndex];
+      ++secondIndex;    
+    }
+    ++copyIndex;
+  }
+
+  while(firstIndex < midPoint) {
+    destination[copyIndex] = values[secondIndex];
+    ++copyIndex;
+    ++firstIndex;
+  }
+
+  while(secondIndex < endIndex) {
+    destination[copyIndex] = values[secondIndex];
+    ++copyIndex;
+    ++secondIndex;
+  }
+  
+  int i;
+  for(int i = 0; i < rangeSize; ++i) {
+    values[i + startIndex] = destination[i];
+  }
+}
+
+private boolean needsSorting(int rangeSize) {
+  return rangeSize >= 2;
 }
 
 
