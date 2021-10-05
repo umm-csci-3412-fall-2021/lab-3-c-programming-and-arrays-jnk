a8bc35977dd73a03bc0ca54f0173fddfc84dfe39
Kyle Day <day00090@stardust.morris.umn.edu>
2021-10-05 10:55:39 -0500

    Add Plan for Writing array_merge in array_merge.c
    This commit comments out a plan for writing the array_merge function in
    array_merge.c. Having the plan commented out will help us remember what
    we're doing later on.
    
    Co-authored-by: Jeffrey Miller mill7707@morris.umn.edu

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -3,5 +3,13 @@
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
   // This is obviously broken. It has the right type, though, eh?
+  /*
+   * IDEA FOR APPROACHING THIS TASK
+   * 1. We merge all of the subarrays together (maybe through iterating through a for loop?)
+   * 2. We run mergesort on the array.
+   * 3. We remove duplicates from the array with another function.
+   * 4. Finally, we get the size of the array and insert said size value into the array.
+   *
+   */
   return sizes;
 }
