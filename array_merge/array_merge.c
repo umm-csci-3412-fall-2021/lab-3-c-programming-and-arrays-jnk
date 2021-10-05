69ead6e64e07c75952d85d8372199f9afe6e6faf
Jeffrey Miller <mill7707@sputnik.morris.umn.edu>
2021-10-05 10:12:59 -0500

    Compile the test function and include mergesort.c
    Adds mergesort.c to the test file so that its functions can be called on as needed.
    
    Co-authored-by: Kyle Day day00090@morris.umn.edu

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,4 +1,5 @@
 #include "array_merge.h"
+#include "../mergesort/mergesort.c"
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
   // This is obviously broken. It has the right type, though, eh?
