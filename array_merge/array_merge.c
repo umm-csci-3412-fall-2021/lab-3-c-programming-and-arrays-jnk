5aadf20dcaa7954afa4a818d2d7d8a8aa2681ca5
Jeffrey Miller <mill7707@sputnik.morris.umn.edu>
2021-10-05 11:19:07 -0500

    Adds adding function
    Adds function that gets the total size of all the concatonated sub-arrays.
    
    Co-authored-by: Kyle Day day00090@morris.umn.edu

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,7 +1,24 @@
 #include "array_merge.h"
 #include "../mergesort/mergesort.c"
 
+int totalSize(int length, int* array){
+  int size = 0;
+  for(int i = 0, i < length, ++i){
+  	size = array[i] + size;
+  }
+  return size
+}
+
+int* mergeSub(int** arr, int* size, int num){
+
+}
+
+
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
+  int total_size;
+
+
   return sizes;
 }
+
+
